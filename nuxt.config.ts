4b66457b979af94ac5ef6116f1c9aa9ca34246fa
aaronthom <hampeaaron@gmail.com>
2025-06-04 09:32:05 +0200

    tailwind now working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,4 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
-  css: ["~/assets/css/tailwind.css"],
 });
