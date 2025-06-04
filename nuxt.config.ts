e43683899f8e54e5acd40388398b7d521772d657
aaronthom <hampeaaron@gmail.com>
2025-06-04 09:46:26 +0200

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  css: ['~/assets/css/main.css'],
 });
